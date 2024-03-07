fc2d66c940b7dec76352b8690b120c9deb052d0a
Tufi007 <taufeeqr346@gmail.com>
2024-03-07 16:46:38 +0530

    try 37

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express=require('express');
 const uxi = require('./uxi');
 const router=express.Router();
 router.get("/",async function(req,res){
-    
+    res.send('ready to go');
 });
 router.get("/a",async function(req,res){
     res.send(`<h1> itchuuuuubfdghbsr</h1>`);
