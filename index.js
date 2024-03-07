2a9b2298da836d0debcec0afe1935ea47f7f67e9
Tufi007 <taufeeqr346@gmail.com>
2024-03-07 16:34:57 +0530

    try 34

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ const router  = require('./routes/route');
 const app=require("./app");
 const PORT = 8011;
 app.use('/',router);
-// app.get('/', (req, res)=>{
-//     res.status(200);
-//     res.send("This is from root tufiiiiii rehmannnnnnnnnnnnnnnnnnnnnnnn");
-// });
-// app.get('/u', (req, res)=>{
-//     res.status(200);
-//     res.send("This is from root tufiiiiiiioioioioiio");
-// });
 
-  
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running and App is listening on port " + PORT);
--- uxi.js
+++ uxi.js
@@ -1,5 +1,5 @@
-const app= require("../app");
-const uxi = require("../routes/uxi");
+const app= require("./app");
+const uxi = require("./routes/uxi");
 const router= require('express').Router();
 app.use('/',uxi);
 module.exports=app;
