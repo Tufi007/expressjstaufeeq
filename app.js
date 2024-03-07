07273dd78bf5843362a5e270d5f0b81dcb24ce53
Tufi007 <taufeeqr346@gmail.com>
2024-03-07 12:03:24 +0530

    try 5

---
+++ app.js
@@ -0,0 +1,5 @@
+const app= require("./index");
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("This is from root tufiiiiii rehmannnnnnnnnnnnnnnnnnnnnnnn");
+});
--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 8011;
   
-app.get('/', (req, res)=>{
-    res.status(200);
-    res.send("This is from root tufiiiiii");
-});
+
   
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("Server is Successfully Running and App is listening on port " + PORT);
     else console.log("Error occurred, server can't start", error);
     }
-);
+);module.exports= app;
