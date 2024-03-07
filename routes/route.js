3f4ebea4b3159d03ff8b3d1afe52d8a5510966e5
Tufi007 <taufeeqr346@gmail.com>
2024-03-07 13:30:47 +0530

    try 25

--- routes/route.js
+++ routes/route.js
@@ -3,4 +3,7 @@ const router=express.Router();
 router.get("/",async function(req,res){
     res.send(`<h1> heloo from rute</h1>`);
 });
+router.get("/u",async function(req,res){
+    res.send(`<h1> uuuuuuuuuuuuuuuuuuuuuuuuuuuu</h1>`);
+});
 module.exports= router;
