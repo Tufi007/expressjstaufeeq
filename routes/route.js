93aadde3eb8f7f6659e524760e1ddd7e39ff2e03
Tufi007 <taufeeqr346@gmail.com>
2024-03-07 16:43:09 +0530

    try 36

--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,7 @@ router.get("/",async function(req,res){
     
 });
 router.get("/a",async function(req,res){
-    res.send(`<h1> itchuuuuu</h1>`);
+    res.send(`<h1> itchuuuuubfdghbsr</h1>`);
 })
 router.use('/',uxi);
 module.exports= router;
